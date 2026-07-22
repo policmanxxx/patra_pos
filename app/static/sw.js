c462d84fd0f9213116b79236b8faedf5c22bf57f
policmanxxx <armyyuwandono@gmail.com>
2026-07-22 14:37:21 +0700

    Inisialisasi awal project Patra_pos

---
+++ app/static/sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'patramojo-v1';
+const urlsToCache = [
+  '/',
+  '/static/logo.jpg',
+  '/manifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch Service Worker (Mode Online-First)
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
